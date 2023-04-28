94825c0c358010cb95ff89041e67a78cdeca5c3e
Gerardo Vargas PPG <gvargas@ppg.com>
2023-04-28 12:34:11 -0600

    Actualización de Scripts

--- Scripts/FND/FND - API Assign Responsibility.sql
+++ Scripts/FND/FND - API Assign Responsibility.sql
@@ -1,5 +1,6 @@
 
 DECLARE
+TYPE t_array_resp_name IS VARRAY(1000) OF VARCHAR2(150);
 --Application Developer
 --System Administrator
 --XML Publisher Administrator
@@ -10,27 +11,29 @@ DECLARE
 --MX CRP INV Config Inventory
 --MX AC GMD Admin Perfiles Seguridad
 --
-p_user_name     VARCHAR2(50):= 'E280938';
-p_resp_name     VARCHAR2(3072):= 
- 'MX AC OM APR Configuracion,'||
- 'MX AC OM FPU Configuracion,'||
- 'MX AC OM PES Configuracion,'||
- 'MX AC OM TPX Configuracion,'||
- 'MX AC OM AGA Configuracion'
+p_user_name     VARCHAR2(50):= 'L625927'; -- y  
+p_resp_name     t_array_resp_name := t_array_resp_name (
+ 'MX AC INV KRO Operador de Interface CWMS'
+,'MX AC INV KRO Operador de Interface'
+,'MX AC INV AGA Operador de Interface'
+,'MX AC INV KRO Operador de Interface CWMS'
+,'MX AC INV KRO Operador de Interface'
+)
 ; 
 
-l_resp_appl_short_name VARCHAR2(100);
-l_responsibility_key VARCHAR2(100);
+l_resp_appl_short_name  VARCHAR2(100);
+l_responsibility_key    VARCHAR2(100);
 l_security_group_key    varchar2(100);
 
 BEGIN
 
 
-    FOR r IN (
+    /*FOR r IN (
             select regexp_substr (p_resp_name, '[^,]+', 1, level) resp
             from dual
             connect by level <= length(regexp_replace(p_resp_name, '[^,]+'))+1 
-        )
+        )*/
+    FOR r IN p_resp_name.first .. p_resp_name.last
     LOOP
         
         begin
@@ -41,7 +44,7 @@ BEGIN
              fnd_application_vl favl
         WHERE 1 =1
           AND frvl.application_id = favl.application_id
-          AND frvl.responsibility_name = R.RESP
+          AND frvl.responsibility_name = p_resp_name (r)
         ;
     
         fnd_user_pkg.addresp ( 
@@ -50,8 +53,8 @@ BEGIN
                         ,resp_key        => l_responsibility_key
                         ,security_group  => 'STANDARD'
                         ,description     => NULL
-                        ,start_date      => sysdate - .5
-                        ,end_date        => sysdate + 365
+                        ,start_date      => sysdate - .1
+                        ,end_date        => sysdate + 120
                     );
         
         COMMIT;
